46a1f651ce3ab2865aaf6299fc438e026f7b6e50
“Chidubem Michael <chidubem@rawdev.com.ng>
2024-02-15 07:25:07 +0100

    feat: egochain initial commit

--- x/evm/keeper/keeper.go
+++ x/evm/keeper/keeper.go
@@ -120,7 +120,7 @@ func (k *Keeper) WithChainID(ctx sdk.Context) {
 	}
 
 	if !(chainID.Cmp(big.NewInt(9001)) == 0 || chainID.Cmp(big.NewInt(9000)) == 0) {
-		panic("EVM only supports Evmos chain identifiers (9000 or 9001)")
+		//panic("EVM only supports Evmos chain identifiers (9000 or 9001)")
 	}
 
 	k.eip155ChainID = chainID
