d3330337712c64389dcc18b63bab30cf17f33a3d
Kingsley GoodluK <goodluckcanhelp@gmail.com>
2024-03-01 19:07:53 +0100

    renamed to egaxd

--- app/app.go
+++ app/app.go
@@ -173,7 +173,7 @@ func init() {
 		panic(err)
 	}
 
-	DefaultNodeHome = filepath.Join(userHomeDir, ".egaxd")
+	DefaultNodeHome = filepath.Join(userHomeDir, ".evmosd")
 
 	// manually update the power reduction by replacing micro (u) -> atto (a) evmos
 	sdk.DefaultPowerReduction = evmostypes.PowerReduction
@@ -185,7 +185,7 @@ func init() {
 }
 
 // Name defines the application binary name
-const Name = "egaxd"
+const Name = "evmosd"
 
 var (
 	// DefaultNodeHome default home directories for the application daemon
